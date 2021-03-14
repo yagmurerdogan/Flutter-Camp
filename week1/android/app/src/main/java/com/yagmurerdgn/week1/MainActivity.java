f02cc584bdb88c165fe6caa35889b56308f4b4b0
Yagmur Erdogan <yagmur.erdogan.1999@gmail.com>
2021-03-14 09:52:42 +0300

    Week 1

---
+++ week1/android/app/src/main/java/com/yagmurerdgn/week1/MainActivity.java
@@ -0,0 +1,6 @@
+package com.yagmurerdgn.week1;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ week1/lib/loop.dart
@@ -0,0 +1,49 @@
+main(List<String> args) {
+
+  /* int number1 = 5;
+
+  String equalMessage = "number is equal to 5";
+  String higherMessage = "number is higher than 5";
+  String lowerMessage  = "number is lower than 5";
+
+  List<String> messages = [equalMessage, higherMessage, lowerMessage];
+
+  if(number1 == 5) {
+    print(messages[0]);
+  } else if(number1 > 5) {
+    print(messages[1]);
+  } else {
+    print(messages[2]);
+  }
+  */
+
+
+  /* int money = 4;
+  List<int> moneys = [5,10,15];
+  moneys[money];
+
+  for( var i = 0; i< moneys.length; i++) {
+    if(moneys[i] == 5) {
+      print ("poor");
+    } else if (moneys[i] == 15) {
+      print("normal");
+    } else {
+      print("rich");
+    }
+  }
+  */
+
+
+  /* List<int> moneys = [5,10,15];
+
+  for( var money in moneys) {
+    if(money == 5) {
+      print ("poor");
+    } else if (money == 15) {
+      print("normal");
+    } else {
+      print("rich");
+    }
+  }
+   */
+}
